d9349d4035588148561da11ae48f14d1021dd4c7
goyalKshitij47 <89925380+goyalKshitij47@users.noreply.github.com>
2022-01-26 16:45:47 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -61,7 +61,7 @@ class Game{
                          fill("red");
                          textSize(20);
                          // add code to display the player name on the respective basket
-                        // text(allPlayers[plr].name,x-32,y+25);
+                     //text(allPlayers[plr].name,x-32,y+25);
 
                          
                      }
